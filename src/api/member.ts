f9ea13798692ef409aefe92c025cf81298ec6d97
yawning5 <yawning055@gmail.com>
2025-05-15 20:12:17 +0900

    delete: member 관련 삭제

--- src/api/member.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import type { Member } from "../types/Member";
-
-const BASE_URL = "http://localhost:8080";
-
-export const fetchMembers = async (): Promise<Member[]> => {
-    const response = await axios.get(`${BASE_URL}/members`);
-        return response.data;
-    };
